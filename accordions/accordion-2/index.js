98c383a842cc136bbeb48b959e19556bdcb80d69
adogbeji <baka_1993@yahoo.co.uk>
2023-02-25 10:16:42 +0000

    Update Notes & start adding JavaScript for Accordion 2

---
+++ accordions/accordion-2/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+
+let acc = document.getElementsByClassName('accordion');
+const promptText = document.querySelector('.accordion__text--1');
+const panel = document.querySelector('.accordion__panel');
