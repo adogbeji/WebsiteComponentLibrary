a0c7a8f0ac1ab668952c29f974c3dec9daa58584
adogbeji <baka_1993@yahoo.co.uk>
2023-02-27 16:31:40 +0000

    Update Notes & add more JavaScript for Accordion 2

--- accordions/accordion-2/index.js
+++ accordions/accordion-2/index.js
@@ -2,5 +2,12 @@
 
 
 let acc = document.getElementsByClassName('accordion');
+const textIconBox = document.querySelector('.accordion__text-icon-box');
 const promptText = document.querySelector('.accordion__text--1');
 const panel = document.querySelector('.accordion__panel');
+
+
+promptText.addEventListener('click', function() {
+    panel.style.height = 'auto';
+    promptText.style.textContent = 'READ LESS';
+});
