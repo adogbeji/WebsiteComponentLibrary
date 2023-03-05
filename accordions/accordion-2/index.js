6c3b303387424c7f82ba60991f973089082d4eab
adogbeji <baka_1993@yahoo.co.uk>
2023-03-05 20:15:54 +0000

    Add JavaScript for Accordion 2

--- accordions/accordion-2/index.js
+++ accordions/accordion-2/index.js
@@ -3,11 +3,19 @@
 
 let acc = document.getElementsByClassName('accordion');
 const textIconBox = document.querySelector('.accordion__text-icon-box');
+const iconBox = document.querySelector('.accordion__icon-box');
 const promptText = document.querySelector('.accordion__text--1');
 const panel = document.querySelector('.accordion__panel');
 
 
-promptText.addEventListener('click', function() {
-        panel.style.height = 'auto';
+textIconBox.addEventListener('click', function() {
+    if (!panel.classList.contains('accordion__panel--show')) {
+        panel.classList.add('accordion__panel--show');
+        iconBox.classList.add('active');
         promptText.textContent = 'Read Less';
+    } else {
+        panel.classList.remove('accordion__panel--show');
+        iconBox.classList.remove('active');
+        promptText.textContent = 'Read More';
+    }
 });
