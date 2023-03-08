8ecaf6821dbaa3b59be522d3253b355f3a43e435
adogbeji <baka_1993@yahoo.co.uk>
2023-03-08 19:08:46 +0000

    Add modifier class & more JavaScript for Accordion 2

--- accordions/accordion-2/index.js
+++ accordions/accordion-2/index.js
@@ -11,10 +11,12 @@ const panel = document.querySelector('.accordion__panel');
 textIconBox.addEventListener('click', function() {
     if (!panel.classList.contains('accordion__panel--show')) {
         panel.classList.add('accordion__panel--show');
+        textIconBox.classList.add('accordion__text-icon-box--move-down');
         iconBox.classList.add('active');
         promptText.textContent = 'Read Less';
     } else {
         panel.classList.remove('accordion__panel--show');
+        textIconBox.classList.remove('accordion__text-icon-box--move-down');
         iconBox.classList.remove('active');
         promptText.textContent = 'Read More';
     }
