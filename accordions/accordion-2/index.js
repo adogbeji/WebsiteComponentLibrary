64d154d2f0121ef6ff20b4c1544e58fc1ef4c235
adogbeji <baka_1993@yahoo.co.uk>
2023-04-07 11:33:17 +0100

    Tweak JavaScript for Accordion 2

--- accordions/accordion-2/index.js
+++ accordions/accordion-2/index.js
@@ -27,21 +27,21 @@
 let accTextIconBox = document.querySelectorAll('.accordion__text-icon-box');
 console.log(accTextIconBox);  // Testing
 let i;
-const accIconBox = document.querySelector('.accordion__icon-box');
-const accPromptText = document.querySelector('.accordion__text--1');
-const accPanel = document.querySelector('.accordion__panel');
+const accIconBox = document.querySelectorAll('.accordion__icon-box');
+const accPromptText = document.querySelectorAll('.accordion__text--1');
+const accPanel = document.querySelectorAll('.accordion__panel');
 
 
 for (i = 0; i < accTextIconBox.length; i++) {
     accTextIconBox[i].addEventListener('click', function() {
         if (!accPanel.classList.contains('accordion__panel--show')) {
             accPanel.classList.add('accordion__panel--show');
-            accTextIconBox.classList.add('accordion__text-icon-box--move-down');
+            accTextIconBox[i].classList.add('accordion__text-icon-box--move-down');
             accIconBox.classList.add('active');
             accPromptText.textContent = 'Read Less';
         } else {
             accPanel.classList.remove('accordion__panel--show');
-            accTextIconBox.classList.remove('accordion__text-icon-box--move-down');
+            accTextIconBox[i].classList.remove('accordion__text-icon-box--move-down');
             accIconBox.classList.remove('active');
             accPromptText.textContent = 'Read More';
         }
