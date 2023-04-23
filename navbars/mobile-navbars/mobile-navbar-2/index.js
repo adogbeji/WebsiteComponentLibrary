2616e3973207768250d34711f2d121a89cfd0cff
adogbeji <baka_1993@yahoo.co.uk>
2023-04-23 19:31:19 +0100

    Add search bar JavaScript for Mobile Navbar 2

--- navbars/mobile-navbars/mobile-navbar-2/index.js
+++ navbars/mobile-navbars/mobile-navbar-2/index.js
@@ -1,6 +1,35 @@
 'use strict';
 
 
+// SEARCH BAR CLOSE ICON
+const closeIcon = document.querySelector('.mobilenav__search-clear-input');
+const input = document.querySelector('.mobilenav__search-input');
+
+// Removes close icon when nothing is typed
+input.addEventListener('input', function(e) {
+  if (input.value !== '') {
+    closeIcon.classList.remove('mobilenav__search-clear-input--hide');
+  } else {
+    closeIcon.classList.add('mobilenav__search-clear-input--hide');
+  }
+});
+
+
+// SEARCH INPUT DROPDOWN BOX 
+const inputDropdownBox = document.querySelector('.mobilenav__search-input-dropdown-box');
+const dropdownContent = document.querySelector('.mobilenav__search-dropdown-content');
+
+input.addEventListener('focus', function() {
+  inputDropdownBox.style.flex = '0 0 75%';
+  dropdownContent.style.minWidth = '38.5rem';
+});
+
+input.addEventListener('focusout', function() {
+  inputDropdownBox.style.flex = '0 0 70%';
+  dropdownContent.style.minWidth = '36rem';
+});
+
+
 // --- FIRST OVERLAY ---
 
 // Slide in from the side
