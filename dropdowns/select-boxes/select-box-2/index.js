e1f601180117d94dede0be0436da915b6a54a3c3
adogbeji <baka_1993@yahoo.co.uk>
2023-06-11 19:20:03 +0100

    Start adding JavaScript for Select Box 2

---
+++ dropdowns/select-boxes/select-box-2/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const selected = document.querySelector('.select-box__prompt');
+const optionsContainer = document.querySelector('.select-box__options');
+
+const optionsList = document.querySelectorAll('.select-box__option');
