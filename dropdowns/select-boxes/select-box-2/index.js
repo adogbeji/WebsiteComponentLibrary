136b097926936e2703f67e82cd05bbb473cbcdd4
adogbeji <baka_1993@yahoo.co.uk>
2023-06-12 11:24:55 +0100

    Add JavaScript for adding and removing .select-box__active class

--- dropdowns/select-boxes/select-box-2/index.js
+++ dropdowns/select-boxes/select-box-2/index.js
@@ -2,5 +2,14 @@
 
 const selected = document.querySelector('.select-box__prompt');
 const optionsContainer = document.querySelector('.select-box__options');
-
 const optionsList = document.querySelectorAll('.select-box__option');
+
+
+// Code for adding and removing .select-box__active class
+selected.addEventListener('click', function() {
+    if (optionsContainer.classList.contains('select-box__active')) {
+        optionsContainer.classList.remove('select-box__active');
+    } else {
+        optionsContainer.classList.add('select-box__active');
+    }
+});
