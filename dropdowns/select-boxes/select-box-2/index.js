be4d16d50fceeac02a04004cc84101ee7558f703
adogbeji <baka_1993@yahoo.co.uk>
2023-06-13 11:27:26 +0100

    Update Notes for Select Box 2

--- dropdowns/select-boxes/select-box-2/index.js
+++ dropdowns/select-boxes/select-box-2/index.js
@@ -7,9 +7,13 @@ const optionsList = document.querySelectorAll('.select-box__option');
 
 // Code for adding and removing .select-box__active class
 selected.addEventListener('click', function() {
-    if (optionsContainer.classList.contains('select-box__active')) {
-        optionsContainer.classList.remove('select-box__active');
-    } else {
-        optionsContainer.classList.add('select-box__active');
-    }
+    // Original Code
+    // if (optionsContainer.classList.contains('select-box__active')) {
+    //     optionsContainer.classList.remove('select-box__active');
+    // } else {
+    //     optionsContainer.classList.add('select-box__active');
+    // }
+
+    // Shortened Code
+    optionsContainer.classList.toggle('select-box__active');
 });
