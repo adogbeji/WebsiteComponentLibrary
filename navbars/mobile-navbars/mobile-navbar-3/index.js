4f5ce169f537a3ed2c802c76a5e76560aad6518f
adogbeji <baka_1993@yahoo.co.uk>
2023-08-04 10:41:00 +0100

    Start adding JavaScript for Mobile Navbar 3

---
+++ navbars/mobile-navbars/mobile-navbar-3/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+// --- FIRST OVERLAY ---
+
+
+// Slide in from the side
+
+/* Open when someone clicks on the SVG element */
+function openMobileMenuOne() {
+    document.getElementById('mobileNavOverlay1').style.width = '100%';
+}
+  
+/* Close when someone clicks on the 'x' symbol inside the overlay */
+const closeIcon = document.querySelector('.mobilenav__search-clear-input');
+const overlay1 = document.querySelector('.mobilenav__overlay-1');
+
+
+closeIcon.addEventListener('click', function() {
+    overlay1.style.width = '0';
+});
