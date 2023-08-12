97ca309176a98fb55859cffcb36f786791701216
adogbeji <baka_1993@yahoo.co.uk>
2023-08-12 11:28:55 +0100

    Add more JavaScript & styling for Mobile Navbar 3

--- navbars/mobile-navbars/mobile-navbar-3/index.js
+++ navbars/mobile-navbars/mobile-navbar-3/index.js
@@ -6,20 +6,31 @@
 
 // Slide in from the side
 
+
+/* Close when someone clicks on the 'x' symbol inside the overlay */
+const overlay1 = document.querySelector('.mobilenav__overlay-1');
+
 /* Open when someone clicks on the SVG element */
 function openMobileMenuOne() {
     document.getElementById('mobileNavOverlay1').style.width = '100%';
 }
-  
-/* Close when someone clicks on the 'x' symbol inside the overlay */
-const overlay1 = document.querySelector('.mobilenav__overlay-1');
-
-
-// closeIcon.addEventListener('click', function() {
-//     overlay1.style.width = '0';
-// });
-
 
 function closeMobileMenuOne() {
     overlay1.style.width = '0';
+}
+
+
+// --- SECOND OVERLAY ---
+
+
+// Slide in from the side
+
+/* Open when someone clicks on the hamburger icon */
+function openMobileMenuTwo() {
+    document.getElementById('mobileNavOverlay2').style.width = '100%';
+}
+
+/* Close when someone clicks on the 'x' symbol inside the overlay */
+function closeMobileMenuTwo() {
+    document.getElementById('mobileNavOverlay2').style.width = '0%';
 }
