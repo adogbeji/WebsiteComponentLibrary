da221c0f9a9e56da43992aca4c141937a88962f2
adogbeji <baka_1993@yahoo.co.uk>
2023-08-15 20:15:14 +0100

    Add more JavaScript for Accordion 1

--- accordions/accordion-1/index.js
+++ accordions/accordion-1/index.js
@@ -41,25 +41,25 @@
 
 
 // Single Accordion
-const accBtn = document.querySelector('.accordion');
-const accPanel = document.querySelector('.accordion__panel');
+// const accBtn = document.querySelector('.accordion');
+// const accPanel = document.querySelector('.accordion__panel');
 
 
-accBtn.addEventListener('click', function() {
-    if (!accPanel.classList.contains('accordion__panel--show')) {
-        accPanel.classList.add('accordion__panel--show');
-        accBtn.classList.add('accordion__active');
-        // accTextIconBox.classList.add('accordion__text-icon-box--move-down');
-        // accIconBox.classList.add('active');
-        // accPromptText.textContent = 'Read Less';
-    } else {
-        accPanel.classList.remove('accordion__panel--show');
-        accBtn.classList.remove('accordion__active');
-        // accTextIconBox.classList.remove('accordion__text-icon-box--move-down');
-        // accIconBox.classList.remove('active');
-        // accPromptText.textContent = 'Read More';
-    }
-});
+// accBtn.addEventListener('click', function() {
+//     if (!accPanel.classList.contains('accordion__panel--show')) {
+//         accPanel.classList.add('accordion__panel--show');
+//         accBtn.classList.add('accordion__active');
+//         // accTextIconBox.classList.add('accordion__text-icon-box--move-down');
+//         // accIconBox.classList.add('active');
+//         // accPromptText.textContent = 'Read Less';
+//     } else {
+//         accPanel.classList.remove('accordion__panel--show');
+//         accBtn.classList.remove('accordion__active');
+//         // accTextIconBox.classList.remove('accordion__text-icon-box--move-down');
+//         // accIconBox.classList.remove('active');
+//         // accPromptText.textContent = 'Read More';
+//     }
+// });
 
 
 
@@ -85,3 +85,28 @@ accBtn.addEventListener('click', function() {
 //         // accPromptText.textContent = 'Read More';
 //     }
 // });
+
+
+
+// Multiple Accordions
+const accBtn = document.querySelectorAll('.accordion');
+const accPanel = document.querySelectorAll('.accordion__panel');
+
+
+for (let i = 0; i < accBtn.length; i++) {
+    accBtn[i].addEventListener('click', function() {
+        if (!accPanel[i].classList.contains('accordion__panel--show')) {
+            accPanel[i].classList.add('accordion__panel--show');
+            accBtn[i].classList.add('accordion__active');
+            // accTextIconBox[i].classList.add('accordion__text-icon-box--move-down');
+            // accIconBox[i].classList.add('active');
+            // accPromptText[i].textContent = 'Read Less';
+        } else {
+            accPanel[i].classList.remove('accordion__panel--show');
+            accBtn[i].classList.remove('accordion__active');
+            // accTextIconBox[i].classList.remove('accordion__text-icon-box--move-down');
+            // accIconBox[i].classList.remove('active');
+            // accPromptText[i].textContent = 'Read More';
+        }
+    });
+}
