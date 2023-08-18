fba0f1ad53abe32b0a8c07ce1d25694d85ca43c8
adogbeji <baka_1993@yahoo.co.uk>
2023-08-18 19:46:55 +0100

    Update JavaScript & styling for Accordion 2

--- accordions/accordion-2/index.js
+++ accordions/accordion-2/index.js
@@ -12,12 +12,12 @@
 //     if (!accPanel.classList.contains('accordion__panel--show')) {
 //         accPanel.classList.add('accordion__panel--show');
 //         accTextIconBox.classList.add('accordion__text-icon-box--move-down');
-//         accIconBox.classList.add('active');
+//         accIconBox.classList.add('accordion__active');
 //         accPromptText.textContent = 'Read Less';
 //     } else {
 //         accPanel.classList.remove('accordion__panel--show');
 //         accTextIconBox.classList.remove('accordion__text-icon-box--move-down');
-//         accIconBox.classList.remove('active');
+//         accIconBox.classList.remove('accordion__active');
 //         accPromptText.textContent = 'Read More';
 //     }
 // });
@@ -37,12 +37,12 @@ for (let i = 0; i < accTextIconBox.length; i++) {
         if (!accPanel[i].classList.contains('accordion__panel--show')) {
             accPanel[i].classList.add('accordion__panel--show');
             accTextIconBox[i].classList.add('accordion__text-icon-box--move-down');
-            accIconBox[i].classList.add('active');
+            accIconBox[i].classList.add('accordion__active');
             accPromptText[i].textContent = 'Read Less';
         } else {
             accPanel[i].classList.remove('accordion__panel--show');
             accTextIconBox[i].classList.remove('accordion__text-icon-box--move-down');
-            accIconBox[i].classList.remove('active');
+            accIconBox[i].classList.remove('accordion__active');
             accPromptText[i].textContent = 'Read More';
         }
     });
