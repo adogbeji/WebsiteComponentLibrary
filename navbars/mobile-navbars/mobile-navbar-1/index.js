b71762dbdedd704671f1719807b3f9db028bdb39
adogbeji <baka_1993@yahoo.co.uk>
2023-08-25 11:07:25 +0100

    Change class names & update JavaScript for Mobile Navbar 1

--- navbars/mobile-navbars/mobile-navbar-1/index.js
+++ navbars/mobile-navbars/mobile-navbar-1/index.js
@@ -57,19 +57,19 @@ function openMobileMenu() {
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function openClickMobileDropDown() {
-    document.getElementById('myDropdown').classList.toggle('mobilenav__dropdown-content--show');
+function openClickMobileDropDownOne() {
+    document.getElementById('mobileNavDropdown_1').classList.toggle('mobilenav__dropdown-content-1--show');
 }
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
-    if (!event.target.matches('.mobilenav__dropdown-btn')) {
-      let dropdowns = document.getElementsByClassName('mobilenav__dropdown-content');
+    if (!event.target.matches('.mobilenav__dropdown-btn-1')) {
+      let dropdowns = document.getElementsByClassName('mobilenav__dropdown-content-1');
       let x;
       for (x = 0; x < dropdowns.length; x++) {
         let openDropdown = dropdowns[x];
-        if (openDropdown.classList.contains('mobilenav__dropdown-content--show')) {
-          openDropdown.classList.remove('mobilenav__dropdown-content--show');
+        if (openDropdown.classList.contains('mobilenav__dropdown-content-1--show')) {
+          openDropdown.classList.remove('mobilenav__dropdown-content-1--show');
         }
       }
     }
