9bd2bcc668ddaf0cfba53d6b528938467cc4cb20
adogbeji <baka_1993@yahoo.co.uk>
2023-08-26 19:36:06 +0100

    Start adding new JavaScript for Carousel 2

--- carousels/carousel-2/index.js
+++ carousels/carousel-2/index.js
@@ -1,30 +1,152 @@
-// let carouselSlideIndex = 1;
-// carouselShowSlides(carouselSlideIndex);
+'use strict';
 
-// // Next/previous controls
-// function carouselPlusSlides(n) {
-//   carouselShowSlides(carouselSlideIndex += n);
-// }
+/* 1) When the right control is pressed, it triggers an event handler that INCREASES the current
+      slide index value by one. It should do this only if the current slide index is less than
+      the maximum value
+*/
 
-// // Thumbnail image controls
-// function carouselCurrentSlide(n) {
-//   carouselShowSlides(carouselSlideIndex = n);
-// }
 
-// function carouselShowSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName('carousel__slides');
-//   let dots = document.getElementsByClassName('carousel__small-photo');
-//   let captionText = document.getElementById('carousel__slide-description');
-//   if (n > slides.length) {carouselSlideIndex = 1}
-//   if (n < 1) {carouselSlideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = 'none';
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(' carousel__active', '');
-//   }
-//   slides[carouselSlideIndex-1].style.display = 'block';
-//   dots[carouselSlideIndex-1].className += ' carousel__active';
-//   captionText.innerHTML = dots[carouselSlideIndex-1].alt;
-// }
+/*  2) When the left control is pressed, it triggers an event handler that DECREASES the current
+       slide index value by one. It should do this only if the current slide index is greater than
+       the maximum value
+*/
+
+
+/*  3) When any of the indicators are pressed, they will display the corresponding slide. For this
+       to work, you need to use a nested for loop
+*/
+
+const slides = document.querySelectorAll('.carousel__slides');  // NodeList containing all slides
+const indicators = document.querySelectorAll('.carousel__dot');  // NodeList containing all indicators
+const prevControl = document.querySelector('.carousel__prev');
+const nextControl = document.querySelector('.carousel__next');
+
+// Minimum and Maximum index value for NodeList (above)
+const minIndexValue = 0;
+const maxIndexValue = slides.length;
+
+// Current index value of slide being shown
+let currentSlidexIndexValue = 0; 
+
+slides[currentSlidexIndexValue].style.display = 'block';
+
+nextControl.addEventListener('click', function() {
+    // alert('Next Control!');
+
+    if (currentSlidexIndexValue < maxIndexValue) {
+        currentSlidexIndexValue += 1;
+        console.log(currentSlidexIndexValue);
+        slides[currentSlidexIndexValue].style.display = 'block';
+    }
+    
+    // Hides right control when current slide index is equal to index value of last item in NodeList
+    if (currentSlidexIndexValue === maxIndexValue - 1) {
+        nextControl.style.cursor = 'not-allowed'; 
+
+        // USE THIS CODE IF YOU WANT TO HIDE CONTROLS
+        // nextControl.style.display = 'none';
+        // prevControl.style.display = 'block';  // Shows previous control
+        console.log(currentSlidexIndexValue);
+    }
+
+    // Shows left control again when current slide index is above the value at which it was closed
+    if (currentSlidexIndexValue > minIndexValue) {
+        prevControl.style.cursor = 'initial';  // Restores cursor to normal state
+
+        // USE THIS CODE IF YOU WANT TO HIDE CONTROLS
+        // prevControl.style.display = 'block';
+        // nextControl.style.display = 'block';  // Shows next control
+        console.log(currentSlidexIndexValue);
+    }
+});
+
+prevControl.addEventListener('click', function() {
+    // alert('Previous Control!');
+
+    if (currentSlidexIndexValue > minIndexValue) {
+        currentSlidexIndexValue -= 1;
+        console.log(currentSlidexIndexValue);
+        slides[currentSlidexIndexValue+1].style.display = 'none';  // Hides previously shown slide
+        slides[currentSlidexIndexValue].style.display = 'block';
+    }
+
+    // Hides left control when current slide index is equal to minimum index value for NodeList
+    if (currentSlidexIndexValue === minIndexValue) {
+        prevControl.style.cursor = 'not-allowed';
+        
+        // USE THIS CODE IF YOU WANT TO HIDE CONTROLS
+        // prevControl.style.display = 'none';
+        // nextControl.style.display = 'block';  // Shows next control
+        console.log(currentSlidexIndexValue);
+    }
+
+    // Shows right control again when current slide index is beneath the value at which it was closed
+    if (currentSlidexIndexValue < maxIndexValue - 1) {
+        nextControl.style.cursor = 'initial';  // Restores cursor to normal state
+        
+        // USE THIS CODE IF YOU WANT TO HIDE CONTROLS
+        // prevControl.style.display = 'none';
+        // nextControl.style.display = 'block';  // Shows next control
+        console.log(currentSlidexIndexValue);
+    }
+});
+
+
+for (let x = 0; x < indicators.length; x++) {
+    indicators[x].addEventListener('click', function() {  // Function is executed whenever an indicator is pressed
+        // alert('Indicators Working!');
+
+        currentSlidexIndexValue = x;  // Sets current slide index to index value of indicator pressed
+
+        // NB: The x value is equivalent to the current slide index in NodeList
+
+        // Hides right control when current x value is equal to index value of last item in NodeList
+        if (currentSlidexIndexValue === maxIndexValue - 1) {
+            nextControl.style.cursor = 'not-allowed'; 
+
+            // USE THIS CODE IF YOU WANT TO HIDE CONTROLS
+            // nextControl.style.display = 'none';
+            // prevControl.style.display = 'block';  // Shows previous control
+            console.log(currentSlidexIndexValue);
+        }
+
+        // Shows left control again when current x value is above the value at which it was closed
+        if (currentSlidexIndexValue > minIndexValue) {
+            prevControl.style.cursor = 'initial';  // Restores cursor to normal state
+
+            // USE THIS CODE IF YOU WANT TO HIDE CONTROLS
+            // prevControl.style.display = 'block';
+            // nextControl.style.display = 'block';  // Shows next control
+            console.log(currentSlidexIndexValue);
+        }
+
+        // Hides left control when current x value is equal to minimum index value for NodeList
+        if (currentSlidexIndexValue === minIndexValue) {
+            prevControl.style.cursor = 'not-allowed';
+        
+            // USE THIS CODE IF YOU WANT TO HIDE CONTROLS
+            // prevControl.style.display = 'none';
+            // nextControl.style.display = 'block';  // Shows next control
+            console.log(currentSlidexIndexValue);
+        }
+
+        // Shows right control again when current a value is beneath the value at which it was closed
+        if (currentSlidexIndexValue < maxIndexValue - 1) {
+            nextControl.style.cursor = 'initial';  // Restores cursor to normal state
+        
+            // USE THIS CODE IF YOU WANT TO HIDE CONTROLS
+            // prevControl.style.display = 'none';
+            // nextControl.style.display = 'block';  // Shows next control
+            console.log(currentSlidexIndexValue);
+        }
+
+        
+        for (let x = 0; x < slides.length; x++) {
+            if (slides[x].style.display === 'block') {  // Looks for current slide which is still visible...
+                slides[x].style.display = 'none';  // It then hides it
+            }
+        }
+
+        slides[x].style.display = 'block';  // Displays slide whose NodeList index value corresponds to indicator pressed 
+    });
+}
