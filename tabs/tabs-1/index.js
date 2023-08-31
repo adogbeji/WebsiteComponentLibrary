136beb853e96915f0ad0c63d495a4fdb31f48da4
adogbeji <baka_1993@yahoo.co.uk>
2023-08-31 20:10:05 +0100

    Start adding JavaScript comments for Tabs 1

--- tabs/tabs-1/index.js
+++ tabs/tabs-1/index.js
@@ -1,22 +1,30 @@
 'use strict';
 
-function openCity(evt, cityName) {
-    // Declare all variables
-    let i, tabcontent, tablinks;
+// function openCity(evt, cityName) {
+//     // Declare all variables
+//     let i, tabcontent, tablinks;
   
-    // Hide all elements with class="tabcontent"
-    tabcontent = document.getElementsByClassName('tab__content');
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = 'none';
-    }
+//     // Hide all elements with class="tabcontent"
+//     tabcontent = document.getElementsByClassName('tab__content');
+//     for (i = 0; i < tabcontent.length; i++) {
+//       tabcontent[i].style.display = 'none';
+//     }
   
-    // Remove "active" class from all elements with class="tab__links"
-    tablinks = document.getElementsByClassName('tab__links');
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace('tab__links-active', '');
-    }
+//     // Remove "active" class from all elements with class="tab__links"
+//     tablinks = document.getElementsByClassName('tab__links');
+//     for (i = 0; i < tablinks.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace('tab__links-active', '');
+//     }
   
-    // Show current tab & add "active" class to button which opened it
-    document.getElementById(cityName).style.display = 'block';
-    evt.currentTarget.className += 'tab__links-active';
-  }
+//     // Show current tab & add "active" class to button which opened it
+//     document.getElementById(cityName).style.display = 'block';
+//     evt.currentTarget.className += 'tab__links-active';
+//   }
+
+
+/* 1) Initially, all the tabs are hidden except one. This is always open by default when the page 
+      loads
+   2) However, when a button is pressed, the corresponding tab opens and the default tab closes
+   3) When another button is pressed, the corresponding tab opens and the previously-opened one
+      closes
+ */
