5929906662700834106fad970d1b2230075b7ef2
adogbeji <baka_1993@yahoo.co.uk>
2023-09-06 19:45:11 +0100

    Add more Notes for Tabs 1

--- tabs/tabs-1/index.js
+++ tabs/tabs-1/index.js
@@ -1,33 +1,12 @@
 'use strict';
 
-// function openCity(evt, cityName) {
-//     // Declare all variables
-//     let i, tabcontent, tablinks;
-  
-//     // Hide all elements with class="tabcontent"
-//     tabcontent = document.getElementsByClassName('tab__content');
-//     for (i = 0; i < tabcontent.length; i++) {
-//       tabcontent[i].style.display = 'none';
-//     }
-  
-//     // Remove "active" class from all elements with class="tab__links"
-//     tablinks = document.getElementsByClassName('tab__links');
-//     for (i = 0; i < tablinks.length; i++) {
-//       tablinks[i].className = tablinks[i].className.replace('tab__links-active', '');
-//     }
-  
-//     // Show current tab & add "active" class to button which opened it
-//     document.getElementById(cityName).style.display = 'block';
-//     evt.currentTarget.className += 'tab__links-active';
-//   }
-
 
 /* 1) Initially, all the tabs are hidden except one. This is always open by default when the page 
       loads
    2) However, when a button is pressed, the corresponding tab opens and the default tab closes
    3) When another button is pressed, the corresponding tab opens and the previously-opened one
       closes
- */
+*/
 
 
 
@@ -41,7 +20,7 @@ tabs[0].classList.add('tab__content--show');
 
 for (let x = 0; x < tabButtons.length; x++) {
    tabButtons[x].addEventListener('click', function() {
-      // Hide currently opened tab here...
+      // Hides previously opened tab
       for (let x = 0; x < tabs.length; x++) {
          if (tabs[x].classList.contains('tab__content--show')) {  // Checks if any of the tabs are already open ...
             tabs[x].classList.remove('tab__content--show'); // In that case, the modifier class is removed
