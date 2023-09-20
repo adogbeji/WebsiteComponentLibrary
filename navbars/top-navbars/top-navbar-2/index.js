e6ec24ebfafb92a1d5cb7acfa87330ff73040897
adogbeji <baka_1993@yahoo.co.uk>
2023-09-20 18:28:44 +0100

    Update JavaScript for Top Navbar 2

--- navbars/top-navbars/top-navbar-2/index.js
+++ navbars/top-navbars/top-navbar-2/index.js
@@ -80,22 +80,22 @@ document.addEventListener('click', function(e) {
 toggle between hiding and showing the dropdown content */
 function openTopnavClickDropDownTwo() {
     // Original Code
-    // if (document.getElementById('topNavDropdown_2').classList.contains('topnav__click-dropdown-content-2--show')) {
-    //     document.getElementById('topNavDropdown_2').classList.remove('topnav__click-dropdown-content-2--show');
+    // if (document.getElementById('topNavDropdown_2').classList.contains('topnav__dropdown-content-2--show')) {
+    //     document.getElementById('topNavDropdown_2').classList.remove('topnav__dropdown-content-2--show');
     // } else {
-    //     document.getElementById('topNavDropdown_2').classList.add('topnav__click-dropdown-content-2--show');
+    //     document.getElementById('topNavDropdown_2').classList.add('topnav__dropdown-content-2--show');
     // }
   
     // Shortened Code
-    document.getElementById('topNavDropdown_2').classList.toggle('topnav__click-dropdown-content-2--show');
+    document.getElementById('topNavDropdown_2').classList.toggle('topnav__dropdown-content-2--show');
 }
 
 document.addEventListener('click', function(e) {
-    if (e.target !== document.querySelector('.topnav__click-dropdown-btn-2')) {
-        const dropdown = document.querySelector('.topnav__click-dropdown-content-2');
+    if (e.target !== document.querySelector('.topnav__dropdown-btn-2')) {
+        const dropdown = document.querySelector('.topnav__dropdown-content-2');
 
-        if (dropdown.classList.contains('topnav__click-dropdown-content-2--show')) {
-            dropdown.classList.remove('topnav__click-dropdown-content-2--show');
+        if (dropdown.classList.contains('topnav__dropdown-content-2--show')) {
+            dropdown.classList.remove('topnav__dropdown-content-2--show');
         }
     }
 });
