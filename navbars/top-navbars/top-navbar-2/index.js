c1afdcfbcff34938d658fef8777d6eddb87906a9
adogbeji <baka_1993@yahoo.co.uk>
2023-10-05 19:13:46 +0100

    Add JavaScript for second mobile dropdown of Top Navbar 2

--- navbars/top-navbars/top-navbar-2/index.js
+++ navbars/top-navbars/top-navbar-2/index.js
@@ -269,3 +269,33 @@ document.addEventListener('click', function(e) {
         }
     }
 });
+
+
+
+// SECOND DROPDOWN:-
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function openClickMobileDropDownTwo() {
+    // Original Code
+    // if (document.getElementById('mobileNavDropdown_2').classList.contains('topnav-mobilenav__dropdown-content-2--show')) {
+    //     document.getElementById('mobileNavDropdown_2').classList.remove('topnav-mobilenav__dropdown-content-2--show');
+    // } else {
+    //     document.getElementById('mobileNavDropdown_2').classList.add('topnav-mobilenav__dropdown-content-2--show');
+    // }
+  
+    // Shortened Code
+    document.getElementById('mobileNavDropdown_2').classList.toggle('topnav-mobilenav__dropdown-content-2--show');
+}
+
+
+// When the user clicks anywhere outside of the dropdown
+document.addEventListener('click', function(e) {
+    if (e.target !== document.querySelector('.topnav-mobilenav__dropdown-btn-2')) {
+        const dropdown = document.querySelector('.topnav-mobilenav__dropdown-content-2');
+
+        if (dropdown.classList.contains('topnav-mobilenav__dropdown-content-2--show')) {
+            dropdown.classList.remove('topnav-mobilenav__dropdown-content-2--show');
+        }
+    }
+});
