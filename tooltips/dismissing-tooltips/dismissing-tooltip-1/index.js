ab8d238e4675e8082df56aed8347faa71a8bd1c5
adogbeji <baka_1993@yahoo.co.uk>
2023-10-10 11:19:23 +0100

    Update otes & add JavaScript comments for Disimssing Tooltip 1

---
+++ tooltips/dismissing-tooltips/dismissing-tooltip-1/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+/* 1) Whenever the user hovers over .dismissing-tooltip__hover-content, the tooltip contents
+      (.dismissing-tooltip__content) should be displayed
+*/
+
+// 2) When the close icon (x) is pressed, the tooltip should disappear
