720da6d52df85c83c0d35c56a5db9a26fcd2b25c
adogbeji <baka_1993@yahoo.co.uk>
2023-10-14 11:07:16 +0100

    Tweak styling for Mobile Navbar 3

--- navbars/mobile-navbars/mobile-navbar-3/index.js
+++ navbars/mobile-navbars/mobile-navbar-3/index.js
@@ -1,36 +1,57 @@
 'use strict';
 
 
-// --- FIRST OVERLAY ---
+// // --- FIRST OVERLAY ---
 
 
-// Slide in from the side
+// // Slide in from the side
 
 
-/* Close when someone clicks on the 'x' symbol inside the overlay */
-const overlay1 = document.querySelector('.mobilenav__overlay-1');
+// /* Close when someone clicks on the 'x' symbol inside the overlay */
+// const overlay1 = document.querySelector('.mobilenav__overlay-1');
 
-/* Open when someone clicks on the SVG element */
-function openMobileMenuOne() {
-    document.getElementById('mobileNavOverlay1').style.width = '100%';
-}
+// /* Open when someone clicks on the SVG element */
+// function openMobileMenuOne() {
+//     document.getElementById('mobileNavOverlay1').style.width = '100%';
+// }
 
-function closeMobileMenuOne() {
-    overlay1.style.width = '0';
-}
+// function closeMobileMenuOne() {
+//     overlay1.style.width = '0';
+// }
+
+
+// // --- SECOND OVERLAY ---
+
+
+// // Slide in from the side
+
+// /* Open when someone clicks on the hamburger icon */
+// function openMobileMenuTwo() {
+//     document.getElementById('mobileNavOverlay2').style.width = '100%';
+// }
+
+// /* Close when someone clicks on the 'x' symbol inside the overlay */
+// function closeMobileMenuTwo() {
+//     document.getElementById('mobileNavOverlay2').style.width = '0%';
+// }
+
 
 
 // --- SECOND OVERLAY ---
 
+const overlay2 = document.querySelector('.mobilenav__overlay-2-content-box');
+const closeButton = document.querySelector('.mobilenav__closebtn-box');
 
-// Slide in from the side
 
-/* Open when someone clicks on the hamburger icon */
-function openMobileMenuTwo() {
-    document.getElementById('mobileNavOverlay2').style.width = '100%';
-}
+closeButton.addEventListener('click', function() {
+    overlay2.classList.add('mobilenav__overlay-2-content-box--hide');
+});
+
 
 /* Close when someone clicks on the 'x' symbol inside the overlay */
-function closeMobileMenuTwo() {
-    document.getElementById('mobileNavOverlay2').style.width = '0%';
+function closeMobileOverlay(a) {
+    alert('Working!');
+    if (a === 2) {
+        overlay2.style.width = '0';
+    }
 }
