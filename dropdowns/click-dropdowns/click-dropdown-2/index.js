686dd6608950e4824a3ca9097b40c4e3ddea89c4
adogbeji <baka_1993@yahoo.co.uk>
2023-11-03 10:42:39 +0000

    Update Notes for Click Dropdown 2

--- dropdowns/click-dropdowns/click-dropdown-2/index.js
+++ dropdowns/click-dropdowns/click-dropdown-2/index.js
@@ -10,13 +10,13 @@ function openClickDropDown() {
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropdown__btn')) {
-      let dropdowns = document.getElementsByClassName('dropdown__content');
-      let x;
-      for (x = 0; x < dropdowns.length; x++) {
-        let openDropdown = dropdowns[x];
-        if (openDropdown.classList.contains('dropdown__content--show')) {
-          openDropdown.classList.remove('dropdown__content--show');
+        let dropdowns = document.getElementsByClassName('dropdown__content');
+        let x;
+        for (x = 0; x < dropdowns.length; x++) {
+            let openDropdown = dropdowns[x];
+            if (openDropdown.classList.contains('dropdown__content--show')) {
+                openDropdown.classList.remove('dropdown__content--show');
+            }
         }
-      }
     }
 }
