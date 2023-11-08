9cc773b03c46f4155c231e91060bccd4a369d159
adogbeji <baka_1993@yahoo.co.uk>
2023-11-08 19:40:33 +0000

    Start adding JavaScript to close header section modal for Course Accordion 1

--- accordions/course-accordions/course-accordion-1/index.js
+++ accordions/course-accordions/course-accordion-1/index.js
@@ -9,3 +9,24 @@ const courseShareModal = document.querySelector('.course-accordion__header-modal
 courseShareButton.addEventListener('click', function() {
     courseShareModal.classList.add('course-accordion__header-modal-visible');
 });
+
+
+// Closes modal (3 possible ways)
+
+// 1) By clicking on the close icon (x) inside the modal:
+const courseShareModalCloseIcon = document.querySelector('.course-accordion__header-modal-closebtn-box');
+
+
+courseShareModalCloseIcon.addEventListener('click', function() {
+    // if (courseShareModal.classList.contains('course-accordion__header-modal-visible')) {
+    //     courseShareModal.classList.remove('course-accordion__header-modal-visible');
+    // }
+    courseShareModal.classList.remove('course-accordion__header-modal-visible');
+});
+
+// 2) By clicking on everything outside of the modal:
+window.onclick = function(e) {
+    if (e.target === courseShareModal) {
+        courseShareModal.classList.remove('course-accordion__header-modal-visible');
+    }
+}
