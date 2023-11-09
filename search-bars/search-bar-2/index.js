aaca98b28975784a08e3a3c909e1a9f153adbbba
adogbeji <baka_1993@yahoo.co.uk>
2023-11-09 19:46:24 +0000

    Start adding JavaScript for Search Bar 2

---
+++ search-bars/search-bar-2/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+// SEARCH BAR
+const searchBar = document.querySelector('.search-bar');
+const searchBarInput = document.querySelector('.search-bar__search-input');
+
+
+searchBarInput.addEventListener('focus', function() {});
