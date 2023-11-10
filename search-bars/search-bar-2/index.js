773690928e49f705a4b6f27e64606f0d1dc2f0c4
adogbeji <baka_1993@yahoo.co.uk>
2023-11-10 19:42:55 +0000

    Add more JavaScript & update Notes for Search Bar 2

--- search-bars/search-bar-2/index.js
+++ search-bars/search-bar-2/index.js
@@ -6,4 +6,10 @@ const searchBar = document.querySelector('.search-bar');
 const searchBarInput = document.querySelector('.search-bar__search-input');
 
 
-searchBarInput.addEventListener('focus', function() {});
+searchBarInput.addEventListener('focus', function() {
+    searchBar.style.width = '100%';
+});
+
+searchBarInput.addEventListener('focusout', function() {
+    searchBar.style.width = '95%';
+});
