4c2ab4df8ccb249ec408bd16758552086325eb72
adogbeji <baka_1993@yahoo.co.uk>
2023-11-16 20:06:32 +0000

    Add more JavaScript for Filter Dropdown 1

--- dropdowns/filter-dropdowns/filter-dropdown-1/index.js
+++ dropdowns/filter-dropdowns/filter-dropdown-1/index.js
@@ -2,9 +2,27 @@
 
 
 function openFilterDropDown() {
-    if (document.getElementById('myDropdown').classList.contains('filter-dropdown__content--show')) {
-        document.getElementById('myDropdown').classList.remove('filter-dropdown__content--show');
-    } else {
-        document.getElementById('myDropdown').classList.add('filter-dropdown__content--show')
+    // Original Code
+    // if (document.getElementById('myDropdown').classList.contains('filter-dropdown__content--show')) {
+    //     document.getElementById('myDropdown').classList.remove('filter-dropdown__content--show');
+    // } else {
+    //     document.getElementById('myDropdown').classList.add('filter-dropdown__content--show')
+    // }
+
+    // Shortened Code
+    document.getElementById('myDropdown').classList.toggle('filter-dropdown__content--show');
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.filter-dropdown__btn')) {
+        let dropdowns = document.getElementsByClassName('filter-dropdown__content');
+        let x;
+        for (x = 0; x < dropdowns.length; x++) {
+            let openDropdown = dropdowns[x];
+            if (openDropdown.classList.contains('filter-dropdown__content--show')) {
+                openDropdown.classList.remove('filter-dropdown__content--show');
+            }
+        }
     }
 }
