2858f014bab9b6b87aebb0333fdd710198533d01
adogbeji <baka_1993@yahoo.co.uk>
2023-11-17 19:16:16 +0000

    Add new JavaScript code for Modal 1

--- modals/modal-1/index.js
+++ modals/modal-1/index.js
@@ -1,67 +1,74 @@
 'use strict';
 
 
-// Makes modal visible
-const openEls = document.querySelectorAll('[data-open]');
-const modal = document.querySelector('.modal');
-const isModalVisible = 'modal__visible';
-
-for(const el of openEls) {
-  el.addEventListener('click', function() {
-    const modalId = this.dataset.open;
-    document.getElementById(modalId).classList.add(isModalVisible);
-  });
-}
-
-
-// Closes modal (3 possible ways)
-
-// 1) By clicking on the custom [data-close] element which is located inside the modal:
-const closeEls = document.querySelectorAll('[data-close]');
-
-for (const el of closeEls) {
-  el.addEventListener('click', function() {
-    this.parentElement.parentElement.parentElement.classList.remove(isModalVisible);
-  });
-}
-
-// 2) By clicking on everything outside of the modal:
-window.onclick = function(e) {
-  if (e.target === modal) {
-    modal.classList.remove(isModalVisible);
-  }
-}
-
-// 3) Lastly by pressing the Esc key:
-document.addEventListener('keydown', function(e) {
-  if (e.key === 'Escape') {
-    modal.classList.remove(isModalVisible);
-  }
-});
-
-
-// --- ALTERNATIVE CODE ---
-
+// // Makes modal visible
+// const openEls = document.querySelectorAll('[data-open]');
 // const modal = document.querySelector('.modal');
-// const modalButton = document.querySelector('.btn');
-// const modalCloseButton = document.querySelector('.modal__closebtn');
+// const isModalVisible = 'modal__visible';
 
-
-// modalButton.addEventListener('click', function() {
-//     modal.classList.add('modal__visible');
-// });
+// for(const el of openEls) {
+//   el.addEventListener('click', function() {
+//     const modalId = this.dataset.open;
+//     document.getElementById(modalId).classList.add(isModalVisible);
+//   });
+// }
 
 
 // // Closes modal (3 possible ways)
 
-// // 1) By clicking on the close icon (x) which is located inside the modal:
-// modalCloseButton.addEventListener('click', function() {
-//     modal.classList.remove('modal__visible');
-// });
+// // 1) By clicking on the custom [data-close] element which is located inside the modal:
+// const closeEls = document.querySelectorAll('[data-close]');
+
+// for (const el of closeEls) {
+//   el.addEventListener('click', function() {
+//     this.parentElement.parentElement.parentElement.classList.remove(isModalVisible);
+//   });
+// }
 
 // // 2) By clicking on everything outside of the modal:
 // window.onclick = function(e) {
-//     if (e.target === modal) {
-//         modal.classList.remove('modal__visible');
-//     }
+//   if (e.target === modal) {
+//     modal.classList.remove(isModalVisible);
+//   }
 // }
+
+// // 3) Lastly by pressing the Esc key:
+// document.addEventListener('keydown', function(e) {
+//   if (e.key === 'Escape') {
+//     modal.classList.remove(isModalVisible);
+//   }
+// });
+
+
+// --- ALTERNATIVE CODE ---
+
+const modal = document.querySelector('.modal');
+const modalButton = document.querySelector('.btn');
+const modalCloseButton = document.querySelector('.modal__closebtn');
+
+
+modalButton.addEventListener('click', function() {
+    modal.classList.add('modal__visible');
+});
+
+
+// Closes modal (3 possible ways)
+
+// 1) By clicking on the close icon (x) which is located inside the modal:
+modalCloseButton.addEventListener('click', function() {
+    modal.classList.remove('modal__visible');
+});
+
+// 2) By clicking on everything outside of the modal:
+window.onclick = function(e) {
+    if (e.target === modal) {
+        modal.classList.remove('modal__visible');
+    }
+}
+
+// 3) Lastly by pressing the Esc key:
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape') {
+        modal.classList.remove('modal__visible');
+    }
+});
