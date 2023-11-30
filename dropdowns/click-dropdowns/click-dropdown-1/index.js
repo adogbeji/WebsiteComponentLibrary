31cdb558ea5659fde9a06facbe4364e15117fac8
adogbeji <baka_1993@yahoo.co.uk>
2023-11-30 10:16:50 +0000

    Add more JavaScript for Click Dropdown 1

--- dropdowns/click-dropdowns/click-dropdown-1/index.js
+++ dropdowns/click-dropdowns/click-dropdown-1/index.js
@@ -2,21 +2,30 @@
 
 
 /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+   toggle between hiding and showing the dropdown content 
+*/
 function openClickDropDown() {
     document.getElementById('myDropdown').classList.toggle('dropdown__content--show');
 }
   
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropdown__btn')) {
-      let dropdowns = document.getElementsByClassName('dropdown__content');
-      let x;
-      for (x = 0; x < dropdowns.length; x++) {
-        let openDropdown = dropdowns[x];
-        if (openDropdown.classList.contains('dropdown__content--show')) {
-          openDropdown.classList.remove('dropdown__content--show');
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(e) {
+    // if (!e.target.matches('.dropdown__btn')) {
+    //     let dropdowns = document.getElementsByClassName('dropdown__content');
+    //     let x;
+    //     for (x = 0; x < dropdowns.length; x++) {
+    //         let openDropdown = dropdowns[x];
+    //         if (openDropdown.classList.contains('dropdown__content--show')) {
+    //             openDropdown.classList.remove('dropdown__content--show');
+    //         }
+    //     }
+    // }
+
+    if (e.target !== document.querySelector('.dropdown__btn')) {
+        const dropdownContent = document.querySelector('.dropdown__content');
+
+        if (dropdownContent.classList.contains('dropdown__content--show')) {
+            dropdownContent.classList.remove('dropdown__content--show');
         }
-      }
     }
-  }
+}
