6731fea358676a9a4471a7e2eef326842bab57b5
adogbeji <baka_1993@yahoo.co.uk>
2024-01-03 12:18:38 +0000

    Add more HTML & JavaScript for second course review modal of Course Accordion 1

--- accordions/course-accordions/course-accordion-1/index.js
+++ accordions/course-accordions/course-accordion-1/index.js
@@ -47,7 +47,7 @@ document.addEventListener('keydown', function(e) {
 
 
 
-// COURSE REVIEW MODAL
+// COURSE REVIEW MODAL #1
 const courseReviewButton = document.querySelector('.course-accordion__header-rating-button');
 const courseReviewModal = document.querySelector('.course-accordion__header-modal-2');
 // const pageBody = document.querySelector('._9714');  // Testing
@@ -579,3 +579,110 @@ starIcon_10a.addEventListener('mouseout', function() {
     // Rating Comment
     ratingText.textContent = 'Select Rating';
 });
+
+
+// COURSE REVIEW MODAL #2
+const reviewBackButton_1 = document.querySelector('.course-accordion__header-modal-3-button-1');
+// const courseReviewButton = document.querySelector('.course-accordion__header-rating-button');
+const courseReviewModal_2 = document.querySelector('.course-accordion__header-modal-3');
+// const pageBody = document.querySelector('._9714');  // Testing
+
+
+reviewBackButton_1.addEventListener('click', function() {
+    // Closes current modal (Review Modal 1)
+    courseReviewModal_2.classList.remove('course-accordion__header-modal-3-visible');
+    pageBody.classList.remove('_9714--remove-scrollbar');
+    
+    // Opens previous modal (Review Modal 1)
+    courseReviewModal.classList.add('course-accordion__header-modal-2-visible');
+    pageBody.classList.add('_9714--remove-scrollbar');  // Testing
+    // pageBody.style.overflowY = 'hidden';
+});
+
+
+// Closes modal (3 possible ways)
+
+// 1) By clicking on the close icon (x) inside the modal:
+const courseReviewModalCloseIcon_2 = document.querySelector('.course-accordion__header-modal-3-closebtn');
+
+
+courseReviewModalCloseIcon_2.addEventListener('click', function() {
+    // if (courseReviewModal.classList.contains('course-accordion__header-modal-2-visible')) {
+    //     courseReviewModal.classList.remove('course-accordion__header-modal-2-visible');
+    //     pageBody.classList.remove('_9714--remove-scrollbar');
+    // }
+    courseReviewModal_2.classList.remove('course-accordion__header-modal-3-visible');
+    pageBody.classList.remove('_9714--remove-scrollbar');
+});
+
+// 2) By clicking on everything outside of the modal:
+window.onclick = function(e) {
+    if (e.target === courseReviewModal_2) {
+        courseReviewModal_2.classList.remove('course-accordion__header-modal-3-visible');
+        pageBody.classList.remove('_9714--remove-scrollbar');
+    }
+}
+
+// 3) Lastly by pressing the Esc key:
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape') {
+        courseReviewModal_2.classList.remove('course-accordion__header-modal-3-visible');
+        pageBody.classList.remove('_9714--remove-scrollbar');
+    }
+});
+
+
+// Star #1
+// const starIcon_1a = document.querySelector('.course-accordion__header-modal-2-icon-1a');
+// const starIcon_1b = document.querySelector('.course-accordion__header-modal-2-icon-1b');
+// const starIcon_2a = document.querySelector('.course-accordion__header-modal-2-icon-2a');
+// const starIcon_2b = document.querySelector('.course-accordion__header-modal-2-icon-2b');
+const starIcon_11a = document.querySelector('.course-accordion__header-modal-3-icon-1a');
+const starIcon_11b = document.querySelector('.course-accordion__header-modal-3-icon-1b');
+const starIcon_12a = document.querySelector('.course-accordion__header-modal-3-icon-2a');
+const starIcon_12b = document.querySelector('.course-accordion__header-modal-3-icon-2b');
+
+// Star #2
+// const starIcon_3a = document.querySelector('.course-accordion__header-modal-2-icon-3a');
+// const starIcon_3b = document.querySelector('.course-accordion__header-modal-2-icon-3b');
+// const starIcon_4a = document.querySelector('.course-accordion__header-modal-2-icon-4a');
+// const starIcon_4b = document.querySelector('.course-accordion__header-modal-2-icon-4b');
+const starIcon_13a = document.querySelector('.course-accordion__header-modal-3-icon-3a');
+const starIcon_13b = document.querySelector('.course-accordion__header-modal-3-icon-3b');
+const starIcon_14a = document.querySelector('.course-accordion__header-modal-3-icon-4a');
+const starIcon_14b = document.querySelector('.course-accordion__header-modal-3-icon-4b');
+
+// Star #3
+// const starIcon_5a = document.querySelector('.course-accordion__header-modal-2-icon-5a');
+// const starIcon_5b = document.querySelector('.course-accordion__header-modal-2-icon-5b');
+// const starIcon_6a = document.querySelector('.course-accordion__header-modal-2-icon-6a');
+// const starIcon_6b = document.querySelector('.course-accordion__header-modal-2-icon-6b');
+const starIcon_15a = document.querySelector('.course-accordion__header-modal-3-icon-5a');
+const starIcon_15b = document.querySelector('.course-accordion__header-modal-3-icon-5b');
+const starIcon_16a = document.querySelector('.course-accordion__header-modal-3-icon-6a');
+const starIcon_16b = document.querySelector('.course-accordion__header-modal-3-icon-6b');
+
+// Star #4
+// const starIcon_7a = document.querySelector('.course-accordion__header-modal-2-icon-7a');
+// const starIcon_7b = document.querySelector('.course-accordion__header-modal-2-icon-7b');
+// const starIcon_8a = document.querySelector('.course-accordion__header-modal-2-icon-8a');
+// const starIcon_8b = document.querySelector('.course-accordion__header-modal-2-icon-8b');
+const starIcon_17a = document.querySelector('.course-accordion__header-modal-3-icon-7a');
+const starIcon_17b = document.querySelector('.course-accordion__header-modal-3-icon-7b');
+const starIcon_18a = document.querySelector('.course-accordion__header-modal-3-icon-8a');
+const starIcon_18b = document.querySelector('.course-accordion__header-modal-3-icon-8b');
+
+// Star #5
+// const starIcon_9a = document.querySelector('.course-accordion__header-modal-2-icon-9a');
+// const starIcon_9b = document.querySelector('.course-accordion__header-modal-2-icon-9b');
+// const starIcon_10a = document.querySelector('.course-accordion__header-modal-2-icon-10a');
+// const starIcon_10b = document.querySelector('.course-accordion__header-modal-2-icon-10b');
+const starIcon_19a = document.querySelector('.course-accordion__header-modal-3-icon-9a');
+const starIcon_19b = document.querySelector('.course-accordion__header-modal-3-icon-9b');
+const starIcon_20a = document.querySelector('.course-accordion__header-modal-3-icon-10a');
+const starIcon_20b = document.querySelector('.course-accordion__header-modal-3-icon-10b');
+
+
+const ratingText_2 = document.querySelector('.course-accordion__header-modal-3-text-2');
+const initialStars_2 = document.querySelector('.course-accordion__header-modal-3-icon-box-1');
+const firstStarRating_2 = document.querySelector('.course-accordion__header-modal-3-icon-box-2');
