dba27050ceb614bb363198d3566c91089cc8c5e8
adogbeji <baka_1993@yahoo.co.uk>
2024-01-04 11:40:52 +0000

    Rearrange dropdown JavaScript fro Mobile Navbar 1

--- navbars/mobile-navbars/mobile-navbar-1/index.js
+++ navbars/mobile-navbars/mobile-navbar-1/index.js
@@ -16,6 +16,52 @@ function openMobileMenu() {
 }
 
 
+// FIRST DROPDOWN
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function openClickMobileDropDownOne() {
+    document.getElementById('mobileNavDropdown_1').classList.toggle('mobilenav__dropdown-content-1--show');
+}
+  
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.mobilenav__dropdown-btn-1')) {
+        let dropdowns = document.getElementsByClassName('mobilenav__dropdown-content-1');
+        let x;
+        for (x = 0; x < dropdowns.length; x++) {
+            let openDropdown = dropdowns[x];
+            if (openDropdown.classList.contains('mobilenav__dropdown-content-1--show')) {
+                openDropdown.classList.remove('mobilenav__dropdown-content-1--show');
+            }
+        }
+    }
+}
+
+
+// SECOND DROPDOWN
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function openClickMobileDropDownTwo() {
+    document.getElementById('mobileNavDropdown_2').classList.toggle('mobilenav__dropdown-content-2--show');
+}
+  
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.mobilenav__dropdown-btn-2')) {
+        let dropdowns = document.getElementsByClassName('mobilenav__dropdown-content-2');
+        let x;
+        for (x = 0; x < dropdowns.length; x++) {
+            let openDropdown = dropdowns[x];
+            if (openDropdown.classList.contains('mobilenav__dropdown-content-2--show')) {
+                openDropdown.classList.remove('mobilenav__dropdown-content-2--show');
+            }
+        }
+    }
+}
+
+
 // SEARCH BAR CLOSE ICON
 const closeIcon = document.querySelector('.mobilenav__search-clear-input');
 const input = document.querySelector('.mobilenav__search-input');
@@ -66,49 +112,3 @@ input.addEventListener('focusout', function() {
   inputDropdownBox.style.flex = '0 0 70%';
   noResultsDropdown.style.minWidth = '31.5rem';
 });
-
-
-// FIRST DROPDOWN
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function openClickMobileDropDownOne() {
-    document.getElementById('mobileNavDropdown_1').classList.toggle('mobilenav__dropdown-content-1--show');
-}
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.mobilenav__dropdown-btn-1')) {
-      let dropdowns = document.getElementsByClassName('mobilenav__dropdown-content-1');
-      let x;
-      for (x = 0; x < dropdowns.length; x++) {
-        let openDropdown = dropdowns[x];
-        if (openDropdown.classList.contains('mobilenav__dropdown-content-1--show')) {
-          openDropdown.classList.remove('mobilenav__dropdown-content-1--show');
-        }
-      }
-    }
-  }
-
-
-// SECOND DROPDOWN
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function openClickMobileDropDownTwo() {
-    document.getElementById('mobileNavDropdown_2').classList.toggle('mobilenav__dropdown-content-2--show');
-}
-  
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.mobilenav__dropdown-btn-2')) {
-        let dropdowns = document.getElementsByClassName('mobilenav__dropdown-content-2');
-        let x;
-        for (x = 0; x < dropdowns.length; x++) {
-            let openDropdown = dropdowns[x];
-            if (openDropdown.classList.contains('mobilenav__dropdown-content-2--show')) {
-                openDropdown.classList.remove('mobilenav__dropdown-content-2--show');
-            }
-        }
-    }
-}
