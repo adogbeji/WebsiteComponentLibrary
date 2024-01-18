2439984ee7efecdd8eb37bb876ece56750b055ba
adogbeji <baka_1993@yahoo.co.uk>
2024-01-18 11:21:34 +0000

    Start adding JavaScript for Dismissing Tooltip 4

---
+++ tooltips/dismissing-tooltips/dismissing-tooltip-4/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+/* 1) Whenever the user hovers over .dismissing-tooltip__hover-content, the tooltip contents
+      (.dismissing-tooltip__content) should be displayed
+*/
+
+/* 2) When the close icon (x) is pressed, the tooltip should disappear
+*/
+
+/* 3) However, when the user hovers over .dismissing-tooltip__hover-content again, the tooltip 
+      contents (.dismissing-tooltip__content) should be displayed once again
+*/
+
+
+const tooltipHoverContent = document.querySelector('.dismissing-tooltip__hover-content');
+const tooltipContent = document.querySelector('.dismissing-tooltip__content');
+const tooltipCloseButton = document.querySelector('.dismissing-tooltip__closebtn');
+
+
+
+tooltipCloseButton.addEventListener('click', function() {  // Hides tooltip when close icon (x) is pressed
+    tooltipContent.classList.add('dismissing-tooltip__content--hide');      
+});
