86b76d461a4de6d034b0908c5fdcb3e90cd1057c
adogbeji <baka_1993@yahoo.co.uk>
2024-01-23 19:34:56 +0000

    Start adding JavaScript for Validation Register Form 1

---
+++ forms/validation-forms/register-forms/validation-register-form-1/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// const form = document.getElementById('form');
+const form = document.querySelector('.form');
+const yourName = document.getElementById('name');
+const email = document.getElementById('email');
+const password_1 = document.getElementById('password_1');
+const password_2 = document.getElementById('password_2');
