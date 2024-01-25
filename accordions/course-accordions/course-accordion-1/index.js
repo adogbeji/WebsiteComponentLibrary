304a295ff2caf964645aca9e6f0d21ce3c9d4b61
adogbeji <baka_1993@yahoo.co.uk>
2024-01-25 18:45:11 +0000

    Add more HTML, styling & JavaScript for modules section of Course Accordion 1

--- accordions/course-accordions/course-accordion-1/index.js
+++ accordions/course-accordions/course-accordion-1/index.js
@@ -1577,3 +1577,66 @@ volumeOffIcon.addEventListener('click', function() {
 });
 
 // --- VOLUME CONTROL ICON END ---
+
+
+// MODULES SECTION
+
+/* 1) Initially, all the tabs are hidden except one. This is always open by default when the page 
+      loads
+*/
+   
+/* 2) However, when a button is pressed, the corresponding tab opens and the default tab closes
+*/
+
+/* 3) When another button is pressed, the corresponding tab opens and the previously-opened one
+      closes
+*/
+
+
+
+const tabButtons = document.querySelectorAll('.course-accordion__modules-tab-button');  // NodeList containing all tab buttons
+const tabs = document.querySelectorAll('.course-accordion__modules-tab-content');  // NodeList containing all tabs
+
+
+// The first tab is shown by default when page loads
+tabs[0].classList.add('course-accordion__modules-tab-content--show');
+
+// The first tab is highlighed by default when page loads (optional)
+tabButtons[0].classList.add('course-accordion__modules-tab-button--darken');  // Highlights corresponding tab
+
+
+for (let x = 0; x < tabButtons.length; x++) {
+    tabButtons[x].addEventListener('click', function() {
+        // Hides previously opened tab
+        for (let x = 0; x < tabs.length; x++) {
+            if (tabs[x].classList.contains('course-accordion__modules-tab-content--show')) {  // Checks if any of the tabs are already open ...
+                tabs[x].classList.remove('course-accordion__modules-tab-content--show'); // In that case, the modifier class is removed
+            }
+        }
+      
+        if (!tabs[x].classList.contains('course-accordion__modules-tab-content--show')) {  // Checks if corresponding tab doesn't contain modifier class
+            tabs[x].classList.add('course-accordion__modules-tab-content--show');  // Opens corresponding tab
+        }
+
+
+        // for (let x = 0; x < tabs.length; x++) {
+        //     if (tabs[x].style.display === 'block') {  // Looks for current slide which is still visible...
+        //         tabs[x].style.display = 'none';  // It then hides it
+        //     }
+        // }
+
+
+        // Highlighting currently opened tab (optional)
+        for (let x = 0; x < tabButtons.length; x++) {
+            if (tabButtons[x].classList.contains('course-accordion__modules-tab-button--darken')) {  // Checks if any of the tabs are already highlighted ...
+                tabButtons[x].classList.remove('course-accordion__modules-tab-button--darken'); // In that case, the modifier class is removed
+            }
+        }
+
+        tabButtons[x].classList.add('course-accordion__modules-tab-button--darken');  // Highlights corresponding tab
+      
+        // if (!tabButtons[x].classList.contains('course-accordion__modules-tab-button--darken')) {  // Checks if corresponding tab doesn't contain modifier class
+        //     tabButtons[x].classList.add('course-accordion__modules-tab-button--darken');  // Highlights corresponding tab
+        // }
+    });
+}
