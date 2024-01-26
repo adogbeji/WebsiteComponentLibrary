31a7258fc85c0270a55d308c6e1c2aac93f5bcb1
adogbeji <baka_1993@yahoo.co.uk>
2024-01-26 10:22:26 +0000

    Start adding JavaScript for langauges modal of Hover Dropdown 3

---
+++ dropdowns/hover-dropdowns/hover-dropdown-3/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+// LANGUAGES MODAL
+const modal = document.querySelector('.dropdown__content-modal');
+const modalButton = document.querySelector('.dropdown__content-languages-button');
+const modalCloseButton = document.querySelector('.dropdown__content-modal-closebtn');
