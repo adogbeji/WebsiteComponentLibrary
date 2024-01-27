0fc8e9b46ac969fcbcb6d16eb22f589b48eb56c2
adogbeji <baka_1993@yahoo.co.uk>
2024-01-27 10:13:40 +0000

    Add more JavaScript for languages modal of Hover Dropdown 3

--- dropdowns/hover-dropdowns/hover-dropdown-3/index.js
+++ dropdowns/hover-dropdowns/hover-dropdown-3/index.js
@@ -5,3 +5,22 @@
 const modal = document.querySelector('.dropdown__content-modal');
 const modalButton = document.querySelector('.dropdown__content-languages-button');
 const modalCloseButton = document.querySelector('.dropdown__content-modal-closebtn');
+
+const modalLanguages = document.querySelectorAll('.dropdown__content-modal-language');
+
+for (let x = 0; x < modalLanguages.length; x++) {
+    modalLanguages[x].addEventListener('click', function() {
+        // modalLanguages[x].classList.add('course-accordion__video-lesson-control-bar-tooltip-dropdown-option--active');
+        // playBackRateTriggerButton.textContent = modalLanguages[x].textContent;
+
+
+        for (let x = 0; x < modalLanguages.length; x++) {
+            if (modalLanguages[x].classList.contains('dropdown__content-modal-language--selected')) {  // Checks if any of the links contains the modifier class
+                modalLanguages[x].classList.remove('dropdown__content-modal-language--selected');  // In that case, the modifier class is removed
+            } 
+        }
+
+        // MOTE: This must come AFTER the for loop above, or else it won't work!
+        modalLanguages[x].classList.add('dropdown__content-modal-language--selected');  // Adds modifier class to mark language button whose NodeList indedx value corresponds to button pressed
+    });
+}
