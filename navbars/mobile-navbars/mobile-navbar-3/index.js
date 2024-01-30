4462ff4668f8ca449c70c2ba9b55ce7ac37da338
adogbeji <baka_1993@yahoo.co.uk>
2024-01-30 11:24:27 +0000

    Ucomment JavaScript & tweak styling for Mobile Navbar 3

--- navbars/mobile-navbars/mobile-navbar-3/index.js
+++ navbars/mobile-navbars/mobile-navbar-3/index.js
@@ -7,17 +7,17 @@
 // // Slide in from the side
 
 
-// /* Close when someone clicks on the 'x' symbol inside the overlay */
-// const overlay1 = document.querySelector('.mobilenav__overlay-1');
+/* Close when someone clicks on the 'x' symbol inside the overlay */
+const overlay1 = document.querySelector('.mobilenav__overlay-1');
 
-// /* Open when someone clicks on the SVG element */
-// function openMobileMenuOne() {
-//     document.getElementById('mobileNavOverlay1').style.width = '100%';
-// }
+/* Open when someone clicks on the SVG element */
+function openMobileMenuOne() {
+    document.getElementById('mobileNavOverlay1').style.width = '100%';
+}
 
-// function closeMobileMenuOne() {
-//     overlay1.style.width = '0';
-// }
+function closeMobileMenuOne() {
+    overlay1.style.width = '0';
+}
 
 
 // // --- SECOND OVERLAY ---
