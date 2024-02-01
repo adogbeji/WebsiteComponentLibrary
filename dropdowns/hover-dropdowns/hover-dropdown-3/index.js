c74db8f05c4f1f76fed24735baa7ac918db2796b
adogbeji <baka_1993@yahoo.co.uk>
2024-02-01 10:12:38 +0000

    Add modal JavaScript for Hover Dropdown 3

--- dropdowns/hover-dropdowns/hover-dropdown-3/index.js
+++ dropdowns/hover-dropdowns/hover-dropdown-3/index.js
@@ -6,20 +6,29 @@ const modal = document.querySelector('.dropdown__content-modal');
 const modalButton = document.querySelector('.dropdown__content-languages-button');
 const modalCloseButton = document.querySelector('.dropdown__content-modal-closebtn');
 
-const modalLanguages = document.querySelectorAll('.dropdown__content-modal-language');
 
-for (let x = 0; x < modalLanguages.length; x++) {
-    modalLanguages[x].addEventListener('click', function() {
-        // modalLanguages[x].classList.add('course-accordion__video-lesson-control-bar-tooltip-dropdown-option--active');
+modalButton.addEventListener('click', function() {
+    modal.classList.add('dropdown__content-modal-visible');
+});
 
 
-        for (let x = 0; x < modalLanguages.length; x++) {
-            if (modalLanguages[x].classList.contains('dropdown__content-modal-language--selected')) {  // Checks if any of the links contains the modifier class
-                modalLanguages[x].classList.remove('dropdown__content-modal-language--selected');  // In that case, the modifier class is removed
-            } 
-        }
+// Closes modal (3 possible ways)
 
-        // MOTE: This must come AFTER the for loop above, or else it won't work!
-        modalLanguages[x].classList.add('dropdown__content-modal-language--selected');  // Adds modifier class to mark language button whose NodeList indedx value corresponds to button pressed
-    });
+// 1) By clicking on the close icon (x) which is located inside the modal:
+modalCloseButton.addEventListener('click', function() {
+    modal.classList.remove('dropdown__content-modal-visible');
+});
+
+// 2) By clicking on everything outside of the modal:
+window.onclick = function(e) {
+    if (e.target === modal) {
+        modal.classList.remove('dropdown__content-modal-visible');
+    }
 }
+
+// 3) Lastly by pressing the Esc key:
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape') {
+        modal.classList.remove('dropdown__content-modal-visible');
+    }
+});
