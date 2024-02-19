4019a159fd0b47a58c9046092425da770a5b5f03
adogbeji <baka_1993@yahoo.co.uk>
2024-02-19 19:08:40 +0000

    Add more JavaScript fro Validation Register Form 1

--- forms/validation-forms/register-forms/validation-register-form-1/index.js
+++ forms/validation-forms/register-forms/validation-register-form-1/index.js
@@ -6,3 +6,72 @@ const yourName = document.getElementById('name');
 const email = document.getElementById('email');
 const password_1 = document.getElementById('password_1');
 const password_2 = document.getElementById('password_2');
+
+
+form.addEventListener('submit', (e) => {
+    checkInputs(e);  // Function that checks form inputs
+});
+
+function checkInputs(e) {
+    // Get values from inputs
+    const nameValue = yourName.value.trim();
+    const emailValue = email.value.trim();
+    const firstPasswordValue = password_1.value.trim();
+    const secondPasswordValue = password_2.value.trim();
+  
+  
+    // Name
+    if (nameValue === '') {
+        e.preventDefault();  // Prevents Form Submission
+        // Show error message + add error class
+        setErrorFor(yourName, 'Name cannot be blank!');
+    } else {
+        // Add success class
+        setSuccessFor(yourName);
+    }
+  
+  
+    // Email
+    if (emailValue === '') {
+        e.preventDefault();  // Prevents Form Submission
+        // Show error message + add error class
+        setErrorFor(email, 'Email cannot be blank!');
+    } else if (!validateEmail(emailValue)) {
+        e.preventDefault();  // Prevents Form Submission
+        setErrorFor(email, 'Email is not valid!');
+    } else {
+        // Add success class
+        setSuccessFor(email);
+    }
+  
+  
+    // Password 1
+    if (firstPasswordValue === '') {
+      e.preventDefault();  // Prevents Form Submission
+      // Show error message + add error class
+      setErrorFor(password_1, 'Password cannot be blank!');
+    } else if (password_1.value.length < 6) {
+      e.preventDefault();  // Prevents Form Submission
+      setErrorFor(password_1, 'Password must be at least 6 characters!');
+    } else if (password_1.value.length >= 20) {
+      e.preventDefault();  // Prevents Form Submission
+      setErrorFor(password_1, 'Password must be less than 20 characters!');
+    } else {
+      // Add success class
+      setSuccessFor(password_1);
+    }
+  
+  
+    // Password 2
+    if (secondPasswordValue === '') {
+      e.preventDefault();  // Prevents Form Submission
+      // Show error message + add error class
+      setErrorFor(password_2, 'Please confirm password!');
+    } else if (firstPasswordValue !== secondPasswordValue) {
+      e.preventDefault();  // Prevents Form Submission
+      setErrorFor(password_2, 'Passwords do not match!');
+    } else {
+      // Add success class
+      setSuccessFor(password_2);
+    }
+}
