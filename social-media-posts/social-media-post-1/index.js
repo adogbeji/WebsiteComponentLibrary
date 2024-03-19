8c0ebb6085a68717e73b6d899c203762cc3b8f8d
adogbeji <baka_1993@yahoo.co.uk>
2024-03-19 18:51:34 +0000

    Add more styling, update Notes & add JavaScript for Social Media Post 1

---
+++ social-media-posts/social-media-post-1/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+const fileUploadButton = document.querySelector('.social-media-post__action-file-upload-button');
+
+
+function uploadFile() {
+    // alert('Working!');
+    fileUploadButton.click();
+}
