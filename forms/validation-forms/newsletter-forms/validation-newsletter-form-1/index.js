adb68e819afdef72834bfb68fa1f17ccc3c8816f
adogbeji <baka_1993@yahoo.co.uk>
2024-06-06 11:05:11 +0100

    Start adding JavaScript for Validation Newsletter Form 1

---
+++ forms/validation-forms/newsletter-forms/validation-newsletter-form-1/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+const yourName = document.getElementById('name');
+const email = document.getElementById('email');
+
+
+form.addEventListener('submit', (e) => {
+    checkInputs(e);  // Function that checks form inputs
+});
