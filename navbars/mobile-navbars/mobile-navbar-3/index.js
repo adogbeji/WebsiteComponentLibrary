d2a0268e575a94b2bf9e55daf43a07451716242a
adogbeji <baka_1993@yahoo.co.uk>
2024-07-02 10:15:55 +0100

    Remove redundant JavaScript code from Mobile Navbar 3

--- navbars/mobile-navbars/mobile-navbar-3/index.js
+++ navbars/mobile-navbars/mobile-navbar-3/index.js
@@ -1,29 +1,10 @@
 'use strict';
 
 
-// // --- FIRST OVERLAY ---
+// --- SECOND OVERLAY ---
 
 
-// // Slide in from the side
-
-
-/* Close when someone clicks on the 'x' symbol inside the overlay */
-const overlay1 = document.querySelector('.mobilenav__overlay-1');
-
-/* Open when someone clicks on the SVG element */
-function openMobileMenuOne() {
-    document.getElementById('mobileNavOverlay1').style.width = '100%';
-}
-
-function closeMobileMenuOne() {
-    overlay1.style.width = '0';
-}
-
-
-// // --- SECOND OVERLAY ---
-
-
-// // Slide in from the side
+// Slide in from the side
 const closeIcon = document.querySelector('.mobilenav__closebtn-box');
 
 // /* Open when someone clicks on the hamburger icon */
