80308bec9d25295485fd2c54dca42265b8ddf37f
adogbeji <baka_1993@yahoo.co.uk>
2024-08-08 10:35:24 +0100

    Add JavaScript for Side Overlay Navar 1

---
+++ navbars/side-overlay-navbars/side-overlay-navbar-1/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById('mySidenav').style.width = '25rem';
+}
+  
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+}
