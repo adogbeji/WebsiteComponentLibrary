7b480cb3f11447ce3a65ffec98c4714f17e484ab
adogbeji <baka_1993@yahoo.co.uk>
2024-09-08 17:01:22 +0100

    Start adding JavaScript for Validation Contact Form 1

---
+++ forms/validation-forms/contact-forms/contact-form-1/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const form = document.querySelector('.form');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
