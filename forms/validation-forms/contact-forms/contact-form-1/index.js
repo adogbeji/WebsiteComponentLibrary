d802442e1dead1f099b61f64e881800fb670fa79
adogbeji <baka_1993@yahoo.co.uk>
2024-09-26 10:28:25 +0100

    Add more JavaScript for Validation Contact Form 1

--- forms/validation-forms/contact-forms/contact-form-1/index.js
+++ forms/validation-forms/contact-forms/contact-form-1/index.js
@@ -91,3 +91,22 @@ function setErrorFor(input, message) {
         formGroup.className = 'form__group';
     }
 }
+
+
+function setSuccessFor(input) {
+    const formGroup = input.parentElement;  // Div with class '.form__group'
+  
+    formGroup.className = 'form__group form__group--success';
+    setTimeout(removeClass, 2500);  // Removes success class after 2.5s
+  
+    function removeClass() {
+        formGroup.className = 'form__group';
+        // formGroup.style.marginBottom = '16px';  // Restores bottom margin
+        formGroup.style.marginBottom = '1.6rem';  // Restores bottom margin
+    }
+}
+
+// Special Function to validate email - returns true or false
+function validateEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
