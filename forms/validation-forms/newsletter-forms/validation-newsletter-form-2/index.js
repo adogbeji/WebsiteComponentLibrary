eabf6a15b7b3c8dc39f3216a3f801bf59f5440a2
adogbeji <baka_1993@yahoo.co.uk>
2024-10-10 10:31:33 +0100

    Add more JavaScript for Validation Newsletter Form 2

--- forms/validation-forms/newsletter-forms/validation-newsletter-form-2/index.js
+++ forms/validation-forms/newsletter-forms/validation-newsletter-form-2/index.js
@@ -2,3 +2,27 @@
 
 const form = document.querySelector('.form');
 const email = document.getElementById('email');
+
+
+form.addEventListener('submit', (e) => {
+    checkInputs(e);  // Function that checks form inputs
+});
+
+function checkInputs(e) {
+    // Get value from input
+    const emailValue = email.value.trim();
+
+
+    // Email
+    if (emailValue === '') {
+        e.preventDefault();  // Prevents Form Submission
+        // Show error message + add error class
+        setErrorFor(email, 'Email cannot be blank!');
+    } else if (!validateEmail(emailValue)) {
+        e.preventDefault();  // Prevents Form Submission
+        setErrorFor(email, 'Email is not valid!');
+    } else {
+        // Add success class
+        setSuccessFor(email);
+    }
+}
