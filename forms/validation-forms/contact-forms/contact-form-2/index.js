b3cb077cd11b3e533e6ca35903396ef300249fcc
adogbeji <baka_1993@yahoo.co.uk>
2024-11-05 10:31:20 +0000

    Add more JavaScript for Contact Form 2

--- forms/validation-forms/contact-forms/contact-form-2/index.js
+++ forms/validation-forms/contact-forms/contact-form-2/index.js
@@ -4,3 +4,10 @@
 const form = document.querySelector('.form');
 const firstName = document.getElementById('firstName');
 const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+
+
+form.addEventListener('submit', (e) => {
+    checkInputs(e);  // Function that checks form inputs
+});
