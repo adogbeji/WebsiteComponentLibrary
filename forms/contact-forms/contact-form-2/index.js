e4b59eb41f9bbc013747e3e4cf8fa38792e2d6fc
adogbeji <baka_1993@yahoo.co.uk>
2024-11-12 10:36:39 +0000

    Add more JavaScript for Contact Form 2

--- forms/contact-forms/contact-form-2/index.js
+++ forms/contact-forms/contact-form-2/index.js
@@ -3,3 +3,14 @@
 
 const accBtn = document.querySelector('.form__accordion');
 const accPanel = document.querySelector('.form__accordion-panel');
+
+
+accBtn.addEventListener('click', function() {
+    if (!accPanel.classList.contains('form__accordion-panel--show')) {
+        accPanel.classList.add('form__accordion-panel--show');
+        accBtn.classList.add('form__accordion-active');
+    } else {
+        accPanel.classList.remove('form__accordion-panel--show');
+        accBtn.classList.remove('form__accordion-active');
+    }
+});
