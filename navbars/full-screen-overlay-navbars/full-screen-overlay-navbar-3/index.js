1ea66178e2a8e4088cfa7063a3fdb9d38746642d
adogbeji <baka_1993@yahoo.co.uk>
2024-11-14 10:20:32 +0000

    Remove testing colours & start adding JavaScript for Full Screen Overlay Navbar 3

---
+++ navbars/full-screen-overlay-navbars/full-screen-overlay-navbar-3/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+// Slide in from the side:-
+
+function openNav() {  /* Open when someone clicks on the span element */
+    document.getElementById('myNav').style.width = '100%';
+}
+  
+function closeNav() {  /* Close when someone clicks on the "x" symbol inside the overlay */
+    document.getElementById('myNav').style.width = '0%';
+}
