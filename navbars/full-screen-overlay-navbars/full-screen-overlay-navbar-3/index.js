7d9a7b4e6ae01d6175fc5343a54b51c52b21dc01
adogbeji <baka_1993@yahoo.co.uk>
2024-11-17 08:56:02 +0000

    Add more JavaScript for Full Screen Overlay Navbar 3

--- navbars/full-screen-overlay-navbars/full-screen-overlay-navbar-3/index.js
+++ navbars/full-screen-overlay-navbars/full-screen-overlay-navbar-3/index.js
@@ -10,3 +10,16 @@ function openNav() {  /* Open when someone clicks on the span element */
 function closeNav() {  /* Close when someone clicks on the "x" symbol inside the overlay */
     document.getElementById('myNav').style.width = '0%';
 }
+
+
+// Slide down from top:-
+
+// NB: For this to work, default '.full-screen-overlay-navbar' height must be 0, width must be 100% & set overflow-y: hidden (except for small-screen)
+
+// function openNav() {  /* Open when someone clicks on the span element */
+//   document.getElementById('myNav').style.height = '100%';
+// }
+
+// function closeNav() {  /* Close when someone clicks on the "x" symbol inside the overlay */
+//   document.getElementById('myNav').style.height = '0%';
+// }
