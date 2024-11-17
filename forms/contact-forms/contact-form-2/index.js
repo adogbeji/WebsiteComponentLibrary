33c7ccc33fadb3fa7cd0e06fd7cce25dc0235255
adogbeji <baka_1993@yahoo.co.uk>
2024-11-17 09:17:45 +0000

    Prevent default form submission for accordion section of Contact Form 2

--- forms/contact-forms/contact-form-2/index.js
+++ forms/contact-forms/contact-form-2/index.js
@@ -5,11 +5,13 @@ const accBtn = document.querySelector('.form__accordion');
 const accPanel = document.querySelector('.form__accordion-panel');
 
 
-accBtn.addEventListener('click', function() {
+accBtn.addEventListener('click', function(e) {
     if (!accPanel.classList.contains('form__accordion-panel--show')) {
+        e.preventDefault();  // Prevents Form Submission
         accPanel.classList.add('form__accordion-panel--show');
         accBtn.classList.add('form__accordion-active');
     } else {
+        e.preventDefault();  // Prevents Form Submission
         accPanel.classList.remove('form__accordion-panel--show');
         accBtn.classList.remove('form__accordion-active');
     }
