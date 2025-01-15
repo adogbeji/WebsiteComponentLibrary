83b109ca45bccab24e7d4c83eb527eb9ca0a1568
adogbeji <baka_1993@yahoo.co.uk>
2025-01-15 10:28:21 +0000

    Change class names for overlay of Top Navbar 3

--- navbars/top-navbars/top-navbar-3/index.js
+++ navbars/top-navbars/top-navbar-3/index.js
@@ -8,12 +8,12 @@ const closeIcon = document.querySelector('.topnav__closebtn-box');
 
 // /* Open when someone clicks on the hamburger icon */
 function openTopnavOverlayOne() {
-    document.getElementById('topnavOverlay1').style.width = '100%';
+    document.getElementById('topnavOverlay').style.width = '100%';
     closeIcon.style.opacity = '1';
 }
 
 // /* Close when someone clicks on the 'x' symbol inside the overlay */
 function closeTopnavOverlay() {
-    document.getElementById('topnavOverlay1').style.width = '0';
+    document.getElementById('topnavOverlay').style.width = '0';
     closeIcon.style.opacity = '0';
 }
