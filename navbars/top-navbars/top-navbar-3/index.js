7de35900ca824e732de6e72733718a4f2279a0d5
adogbeji <baka_1993@yahoo.co.uk>
2025-02-25 10:28:50 +0000

    Add more JavaScript for Top Navbar 3

--- navbars/top-navbars/top-navbar-3/index.js
+++ navbars/top-navbars/top-navbar-3/index.js
@@ -24,6 +24,7 @@ const button_1 = document.querySelector('.topnav__overlay-1-button:first-child')
 const button_2 = document.querySelector('.topnav__overlay-1-button:nth-child(2)');
 const button_3 = document.querySelector('.topnav__overlay-1-button:nth-child(3)');
 const button_4 = document.querySelector('.topnav__overlay-1-button:nth-child(4)');
+const button_5 = document.querySelector('.topnav__overlay-1-button:nth-child(5)');
 const overlayLinkBox_1 = document.querySelector('.topnav__overlay-1-link-box:nth-child(2)');
 const overlayLinkBox_2 = document.querySelector('.topnav__overlay-1-link-box:nth-child(3)');
 const overlayLinkBox_3 = document.querySelector('.topnav__overlay-1-link-box:nth-child(4)');
@@ -45,4 +46,28 @@ button_2.addEventListener('click', function() {
     overlayLinkBox_3.classList.add('topnav__overlay-1-link-box--hide');
     overlayLinkBox_4.classList.add('topnav__overlay-1-link-box--hide');
     overlayLinkBox_5.classList.add('topnav__overlay-1-link-box--hide');
+});
+
+button_3.addEventListener('click', function() {
+    overlayLinkBox_1.classList.add('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_2.classList.add('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_3.classList.remove('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_4.classList.add('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_5.classList.add('topnav__overlay-1-link-box--hide');
+});
+
+button_4.addEventListener('click', function() {
+    overlayLinkBox_1.classList.add('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_2.classList.add('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_3.classList.add('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_4.classList.remove('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_5.classList.add('topnav__overlay-1-link-box--hide');
+});
+
+button_5.addEventListener('click', function() {
+    overlayLinkBox_1.classList.add('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_2.classList.add('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_3.classList.add('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_4.classList.add('topnav__overlay-1-link-box--hide');
+    overlayLinkBox_5.classList.remove('topnav__overlay-1-link-box--hide');
 });
